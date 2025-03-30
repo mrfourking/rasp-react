ece262b9447601b9b070c6a031829ec231365306
Ilya Fokin <ifokin@kr.digital>
2025-03-30 09:32:34 +0300

    chore: update linters

--- src/components/Calendar/Month.tsx
+++ src/components/Calendar/Month.tsx
@@ -1,45 +1,45 @@
-import { Box, SimpleGrid } from '@chakra-ui/react'
-import { FC, useMemo } from 'react'
+import { Box, SimpleGrid } from "@chakra-ui/react"
+import { FC, useMemo } from "react"
 
-import DayCell from './DayCell'
+import DayCell from "./DayCell"
 
-import { IMonthProps } from '../../types'
+import { IMonthProps } from "@src/types"
 
 const Month: FC<IMonthProps> = ({ date, datesArray }) => {
   const generatedDays = useMemo(() => {
     const days = []
-    let currentDate = date.startOf('month')
-    const endDate = date.endOf('month')
+    let currentDate = date.startOf("month")
+    const endDate = date.endOf("month")
     const startWeekday = currentDate.weekday
     const endWeekDay = endDate.weekday
 
     for (let i = 0; i < startWeekday - 1; i++) {
-      days.push(<DayCell key={`${currentDate.toFormat('LL')}-start-${i}`} />)
+      days.push(<DayCell key={`${currentDate.toFormat("LL")}-start-${i}`} />)
     }
 
     while (currentDate < endDate) {
       days.push(
         <DayCell
-          key={currentDate.toFormat('dd.MM.yyyy')}
-          selected={datesArray.includes(currentDate.toFormat('dd.MM.yyyy'))}
+          key={currentDate.toFormat("dd.MM.yyyy")}
+          selected={datesArray.includes(currentDate.toFormat("dd.MM.yyyy"))}
         >
-          {currentDate.toFormat('dd')}
-        </DayCell>
+          {currentDate.toFormat("dd")}
+        </DayCell>,
       )
 
       currentDate = currentDate.plus({ day: 1 })
     }
 
     for (let i = endWeekDay; i < 7; i++) {
-      days.push(<DayCell key={`${currentDate.toFormat('LL')}-end-${i}`} />)
+      days.push(<DayCell key={`${currentDate.toFormat("LL")}-end-${i}`} />)
     }
 
     return days
-  }, [date])
+  }, [date, datesArray])
 
   return (
-    <Box width={'100%'} marginBottom="40px">
-      <Box>{date.toFormat('LLLL yyyy')}</Box>
+    <Box width={"100%"} marginBottom="40px">
+      <Box>{date.toFormat("LLLL yyyy")}</Box>
       <SimpleGrid columns={7}>
         <DayCell height="24px">Пн</DayCell>
         <DayCell height="24px">Вт</DayCell>
